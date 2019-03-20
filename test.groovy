3c4fc6a754fa1b43e08dde0a2e4b2b7e1b642325
jleleu42 <jlu59@live.fr>
2019-03-20 10:19:22 +0100

    10

--- test.groovy
+++ test.groovy
@@ -9,6 +9,8 @@ job('My Job php') {
         scm('H/5 * * * *')
     }
     steps {
+        shell("cd php")
+        shell("ls")
         shell("composer install")
         shell("./php/vendor/bin/phpunit test.php")
     }
